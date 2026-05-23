fc71a1f712f4875ee502f4c41d89ba08eb9b64b6
ceylan-karabay <ceylan_karabay@outlook.com>
2026-05-23 21:27:34 +0300

    Update proje

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Point {
+    private int x;
+    private int y;
+    public  Point(int x, int y){
+        setX(x);
+        setY(y);
+    }
+    public int getX(){
+        return  x;
+    }
+    public int getY(){
+        return  y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public double distance() {
+        return Math.sqrt((x * x) + (y * y));
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt(
+                ((p.x - this.x) * (p.x - this.x)) +
+                ((p.y - this.y) * (p.y - this.y))
+      );
+    }
+    public double distance(int a, int b) {
+        return Math.sqrt(
+                ((a - this.x) * (a - this.x)) +
+                        ((b - this.y) * (b - this.y))
+        );
+    }
+}
